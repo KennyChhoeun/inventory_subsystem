3bb18b422d405690d090cd9da78b8b0f27de7968
Kenny Chhoeun <kchhoeun20703@gmail.com>
2020-04-11 14:50:30 -0700

    initial commit

---
+++ src/inventorysubsystem/Item.java
@@ -0,0 +1,65 @@
+package inventorysubsystem;
+/**
+ *
+ * @author inventory subsytem team
+ */
+public class Item implements ItemInterface {
+    
+    int productID, amountAvailable;
+    String productName, backroomLocation, salesfloorLocation;
+    boolean onSale;
+
+    @Override
+    public void addAnItem(int productID) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+        //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removeAnItem(int productID) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+        //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void defectAnItem(int productID) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+        //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void putItemOnSale(double oldPrice, double newPrice) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+        //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void searchItem(int productID, String itemName) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+        //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isOnSale(int productID, String itemName) {
+        // return true if product onSale == true
+        return this.onSale;
+    }
+
+    @Override
+    public void relocateItem(int productID, String itemName, String newLocation) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+        //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void restock(int amountRestocked) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+        //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void checkPrice(int productID, String itemName) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+        //To change body of generated methods, choose Tools | Templates.
+    }
+}
---
+++ src/inventorysubsystem/ItemInterface.java
@@ -0,0 +1,17 @@
+package inventorysubsystem;
+
+/**
+ *
+ * @author inventory subsystem team
+ */
+public interface ItemInterface {
+    public void addAnItem(int productID);
+    public void removeAnItem(int productID);
+    public void defectAnItem(int productID);
+    public void putItemOnSale(double oldPrice, double newPrice);
+    public void searchItem(int productID, String itemName);
+    public boolean isOnSale(int productID, String itemName);
+    public void relocateItem(int productID, String itemName, String newLocation);
+    public void restock(int amountRestocked);
+    public void checkPrice(int productID, String itemName);
+}
